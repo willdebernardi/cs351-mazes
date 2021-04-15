52fc85ee93cd798f3be4d7ec23b2e68697dbfb07
Christopher Medlin <cmedlin@unm.edu>
2021-04-15 13:00:20 -0600

    Fix the double green square bug

--- src/main/java/solvers/MazeSolver.java
+++ src/main/java/solvers/MazeSolver.java
@@ -7,12 +7,15 @@ package solvers;
 
 import gui.Display;
 import javafx.scene.paint.Color;
+import maze.Direction;
 import maze.Maze;
+import maze.MazeState;
 import maze.Vertex;
 
 public abstract class MazeSolver {
     private Display display;
     private Vertex lastVisited;
+    private Direction dir;
 
     /**
      * Create a new MazeSolver
@@ -38,10 +41,8 @@ public abstract class MazeSolver {
      */
     public void visit(Vertex v) {
         if (this.display != null) {
-            this.display.updateSolver("", v);
-            // paint the last visited cell back to white
             if (lastVisited != null) {
-                this.display.cellsChanged(Color.LIGHTGREEN, lastVisited);
+                this.display.updateSolver(v, lastVisited);
             }
             this.lastVisited = v;
         }
@@ -75,5 +76,50 @@ public abstract class MazeSolver {
      */
     protected Display getDisplay() {
         return this.display;
+    }
+
+    public void initDirection(Vertex startVertex) {
+        Direction entranceDirection = null;
+
+        Vertex current = startVertex;
+        if(current.getEdge(Direction.LEFT).getState() == MazeState.ENTRANCE ||
+                current.getEdge(Direction.LEFT).getState() == MazeState.EXIT) {
+            entranceDirection = Direction.LEFT;
+        } else if (current.getEdge(Direction.UP).getState() == MazeState.ENTRANCE ||
+                current.getEdge(Direction.UP).getState() == MazeState.EXIT) {
+            entranceDirection = Direction.UP;
+        } else if (current.getEdge(Direction.RIGHT).getState() == MazeState.ENTRANCE ||
+                current.getEdge(Direction.RIGHT).getState() == MazeState.EXIT) {
+            entranceDirection = Direction.RIGHT;
+        } else if (current.getEdge(Direction.DOWN).getState() == MazeState.ENTRANCE ||
+                current.getEdge(Direction.DOWN).getState() == MazeState.EXIT) {
+            entranceDirection = Direction.DOWN;
+        }
+        this.dir = entranceDirection.reverse();
+    }
+
+    public Direction getCurrentDirection() {
+        return this.dir;
+    }
+
+    /**
+     * Turns the solver in the given direction.
+     *
+     * @param d the direction
+     */
+    public void turn(Direction d) {
+       switch (d) {
+           case UP:
+               break;
+           case RIGHT:
+               this.dir = this.dir.turnRight();
+               break;
+           case LEFT:
+               this.dir = this.dir.turnLeft();
+               break;
+           case DOWN:
+               this.dir = this.dir.reverse();
+               break;
+       }
     }
 }
